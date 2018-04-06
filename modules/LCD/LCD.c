de0eff58622306c1221050e480a7a5f8c4602ba6
ZK <Zhengkai.Z.Wang@gmail.com>
2018-04-06 21:23:44 +0800

    finish LCD module& error_handling

---
+++ modules/LCD/LCD.c
@@ -0,0 +1,17 @@
+#include "LCD.h"
+#include <project.h>
+
+void LCD_init(){
+    LCD_Char_Start();
+    LCD_Char_LoadCustomFonts(LCD_Char_customFonts);
+
+}
+
+void LCD_disp(const char* str){
+    LCD_Char_Position(0, 0);
+    LCD_Char_PrintString("error: ");
+    LCD_Char_Position(1, 0);
+    LCD_Char_PrintString("        ");
+    LCD_Char_Position(1, 0);
+    LCD_Char_PrintString(str);
+}
---
+++ modules/LCD/LCD.h
@@ -0,0 +1,8 @@
+#ifndef LCD_H_
+#define LCD_H_
+
+void LCD_init();  //initialize LCD
+
+void LCD_disp(const char* str);  //display string on LCD
+
+#endif
